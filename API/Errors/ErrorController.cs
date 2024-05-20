e8850cdc25f0b9fa4e629907979d9f641e6a6785
r1nzl3rrr <khanhle9344@gmail.com>
2024-05-20 11:35:38 +0700

    Clean Architecture Project Structure

---
+++ API/Errors/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Errors
+{
+    [Route("errors/{code}")]
+    public class ErrorController : BaseApiController
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+    }
+}
