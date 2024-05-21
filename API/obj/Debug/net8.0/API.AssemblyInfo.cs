d6f16913dd2799bc219420d6015902d209679c05
r1nzl3rrr <khanhle9344@gmail.com>
2024-05-21 08:51:27 +0700

    Update default connection string

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5413afc4d7f8308b3dfaa3bf5d6684b39b7bd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f401c4800ba8cdfa0b53346af56bbfb313f6e59c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
