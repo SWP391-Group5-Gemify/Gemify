358cb4d808ffeb022b70acbc0e29a42085095167
r1nzl3rrr <khanhle9344@gmail.com>
2024-05-22 16:43:04 +0700

    Added generic repository

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Core.Enitities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _storeContext;
+
+        public GenericRepository(StoreContext storeContext) 
+        {
+            _storeContext = storeContext;
+        }
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _storeContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _storeContext.Set<T>().FindAsync(id);
+        }
+
+        public void Add(T entity)
+        {
+            _storeContext.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _storeContext?.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _storeContext.Set<T>().Attach(entity);
+            _storeContext.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
