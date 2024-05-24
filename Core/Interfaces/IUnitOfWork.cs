1ecfe44e9c48e7313998f9269cf7dc28083b9606
r1nzl3rrr <khanhle9344@gmail.com>
2024-05-24 11:29:37 +0700

    Khanh Le - Apply Unit of Work pattern #22

---
+++ Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Core.Enitities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    /// <summary>
+    /// This interface defines the Unit Of Work (UoW) pattern for data access. 
+    /// It provides methods for managing a transaction scope, 
+    /// interacting with repositories, and ensuring proper resource disposal.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> Repository<T>() where T : BaseEntity;
+        Task<int> Complete();
+    }
+}
