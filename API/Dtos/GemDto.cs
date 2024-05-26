074e906905528c43430353fea4e07504f0ad737a
meewaldor <tieumylam91@gmail.com>
2024-05-26 21:16:30 +0700

    My - Added ProductDto and GemDto #36

---
+++ API/Dtos/GemDto.cs
@@ -0,0 +1,21 @@
+﻿using Core.Enitities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class GemDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Proportion { get; set; }
+        public string Polish { get; set; }
+        public string Symmetry { get; set; }
+        public float Carat { get; set; }
+        public string Cut { get; set; }
+        public string Clarity { get; set; }
+        public string Color { get; set; }
+        public string Shape { get; set; }
+        public float LatestPrice { get; set; }
+    }
+}
---
+++ API/Dtos/ProductDto.cs
@@ -0,0 +1,23 @@
+﻿using Core.Enitities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string GoldType { get; set; }
+        public float GoldWeight { get; set; }
+        public float TotalWeight { get; set; }
+        public float Labour { get; set; }
+        public string Status { get; set; }
+        public int Quantity { get; set; }
+        public string ImageUrl { get; set; }
+        public string SubCategoryName { get; set; }
+        public string CategoryName { get; set; }
+        public string SaleCounterName { get; set; }
+        public IReadOnlyList<GemDto> Gems { get; set; }
+    }
+}
