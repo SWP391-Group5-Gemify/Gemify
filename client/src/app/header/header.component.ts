1a0adfb08e26bad6fc296a87a99b11ea94918bee
SE182407_VuKimDuy <duyvkse182407@fpt.edu.vn>
2024-05-27 11:38:18 +0700

    Duy - Add Merriweather and button components #27

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -14,12 +15,12 @@ export class HeaderComponent {
 
   // Links
   navLinks = [
-    { id: 1, name: 'Home' },
-    { id: 2, name: 'Live Gold Chart' },
+    { id: 1, name: 'Home', route: '/home' },
+    { id: 2, name: 'Live Gold Chart', route: '/liveGold' },
   ];
 
   // Login
-  loginLinks = { id: 1, name: 'Login' };
+  loginLinks = { id: 1, name: 'Login', route: '/login' };
 
   // Logo
   logo = { id: 1, name: 'Gemify', src: '' };
