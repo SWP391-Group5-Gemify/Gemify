15ea1c2f3c78fecff4df97ffc2aa77c2852d1bb6
meewaldor <tieumylam91@gmail.com>
2024-05-27 11:46:13 +0700

    My - Added product count specification #36

---
+++ Core/Specifications/ProductCountSpecification.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Core.Enitities;
+
+namespace Core.Specifications
+{
+    public class ProductCountSpecification : BaseSpecification<Product>
+    {
+        public ProductCountSpecification(ProductParams productParams)
+        : base (x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                (productParams.Search)) &&
+                (string.IsNullOrEmpty(productParams.Status) || x.Status.ToString() == productParams.Status) &&
+                (!productParams.GoldTypeId.HasValue || x.GoldTypeId == productParams.GoldTypeId) &&
+                (!productParams.SubCategoryId.HasValue || x.GoldTypeId == productParams.SubCategoryId) &&
+                (!productParams.CategoryId.HasValue || x.GoldTypeId == productParams.CategoryId))
+        {
+
+        }
+    }
+}
