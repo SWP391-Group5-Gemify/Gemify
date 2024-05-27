ff55dde3e1c4bc98fc9a6f80f01261f17c3270bf
SE182407_VuKimDuy <duyvkse182407@fpt.edu.vn>
2024-05-27 10:14:17 +0700

    Duy - Add 7-in-1 SCSS Architecture #27

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'client';
--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -5,8 +5,22 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  // ================================
+  // == Normal + Authenticated Users
+  // ================================
 
+  // Links
+  navLinks = [
+    { id: 1, name: 'Home' },
+    { id: 2, name: 'Live Gold Chart' },
+  ];
+
+  // Login
+  loginLinks = { id: 1, name: 'Login' };
+
+  // Logo
+  logo = { id: 1, name: 'Gemify', src: '' };
 }
