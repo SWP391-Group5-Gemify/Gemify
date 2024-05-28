115175198cc054d502bfdd3a6c287b2a7e32aef8
SE182407_VuKimDuy <duyvkse182407@fpt.edu.vn>
2024-05-28 11:15:59 +0700

    Duy - Add Login with Errors #6

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import {
   ReactiveFormsModule,
   FormControl,
   FormBuilder,
+  Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -29,6 +30,10 @@ import { CommonModule } from '@angular/common';
 })
 export class LoginComponent implements OnInit {
   signInForm?: FormGroup;
+  contact = {
+    text: 'If you forgot your password. Please contact admin',
+    number: '0909 312 423',
+  };
 
   // Inject FormBuilder Service into the constructor
   constructor(private formBuilder: FormBuilder) {}
@@ -36,19 +41,54 @@ export class LoginComponent implements OnInit {
   // On Init
   ngOnInit(): void {
     this.signInForm = this.formBuilder.group({
-      username: new FormControl(''), // default value change in username
-      password: new FormControl(''), // default value change in password
-    });
+      // required, min length is 6, max is 32, receive a to z, case insensitive
+      username: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(/^[a-z]{6,32}$/i),
+        ])
+      ),
 
-    // setTimeout(() => {
-    //   this.signInForm?.reset;
-    //   this.signInForm?.patchValue({
-    //     username: 'Vu Kim Duy',
-    //   });
-    // }, 500);
+      // at least 1 special appear anywhere, a to z, 0 to 9, min 6 and max 32
+      password: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(/^(?=.*[!@#$%^&*]+)[a-z0-9!@#$%^&*]{6,32}$/),
+        ])
+      ),
+    });
   }
 
-  onSubmit() {
-    console.log(this.signInForm);
+  // Checking if the input is valid or not for mat-errors
+  getErrorMessageOnControl(controlName: string): string {
+    const control = this.signInForm!.get(controlName);
+
+    let errorMessage: string = '';
+
+    switch (true) {
+      case control?.hasError('required'):
+        errorMessage = 'This field is required';
+        break;
+      case control?.hasError('minlength'):
+        errorMessage = 'Must be at least 6 characters long';
+        break;
+      case control?.hasError('pattern'):
+        if (controlName === 'username') {
+          ('Username must contain only letters and be between 6 and 32 characters long');
+        } else {
+          ('Password must contain at least one special character and be between 6 and 32 characters long');
+        }
+        break;
+    }
+    return errorMessage;
+  }
+
+  // On Submit Form
+  login(): void {
+    this.signInForm?.value.username;
   }
 }
