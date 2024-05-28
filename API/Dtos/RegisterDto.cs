04c043680c2508cc87389c4b1d4f0d792c96ebd7
r1nzl3rrr <khanhle9344@gmail.com>
2024-05-28 14:47:40 +0700

    Added constraints for username, email and password

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,11 +1,22 @@
 ﻿using Core.Enitities;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
+        [RegularExpression("^([a-zA-Z])[a-zA-Z_-]*[\\w_-]*[\\S]$|^([a-zA-Z])[0-9_-]*[\\S]$|^[a-zA-Z]*[\\S]$", 
+            ErrorMessage = "Username must start with a Alphabet, no space, no special characters except for underscore and dash")]
         public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+            ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and at least 6 characters")]
         public string Password { get; set; }
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
