989b49951f4b6b428c93955c95d6627a7799fa7e
meewaldor <tieumylam91@gmail.com>
2024-05-30 13:15:07 +0700

    My - Removed unused method #36

--- Infrastructure/Services/ProductService.cs
+++ Infrastructure/Services/ProductService.cs
@@ -58,19 +58,5 @@ namespace Infrastructure.Services
             var result = await _unitOfWork.Complete();
             return result > 0;
         }
-
-        public float CalculateGemsPrice(Product p)
-        {
-            var gemsPrice = (float) 0.0;
-            foreach (ProductGem pg in p.ProductGems)
-            {
-                gemsPrice += pg.GemType.LatestPrice * pg.Quantity;
-            }
-            return gemsPrice;
-        }
-        public float CalculateProductPrice(Product p)
-        {
-            return (float)(p.GoldType.LatestBidPrice * p.GoldWeight + CalculateGemsPrice(p));
-        }
     }
 }
