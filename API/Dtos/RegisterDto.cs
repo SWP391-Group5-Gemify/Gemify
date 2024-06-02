d3be3545a4f9f1af71b54693fba4f7042f22b5a3
r1nzl3rrr <khanhle9344@gmail.com>
2024-06-02 09:31:41 +0700

    Changed password regex

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -16,8 +16,8 @@ namespace API.Dtos
         public string Email { get; set; }
 
         [Required]
-        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
-            ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and at least 6 characters")]
+        [RegularExpression("^(?=.*\\d).{8,15}$", 
+            ErrorMessage = "Password must be between 8 and 15 digits long and include at least one numeric digit")]
         public string Password { get; set; }
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
