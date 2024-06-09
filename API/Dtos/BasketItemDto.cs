76b3afbcf72e268c95ae378f48009b0824967c7c
anhphan237 <anh713988@gmail.com>
2024-06-10 00:17:33 +0700

    Q.Anh - Created Basket Management #84

---
+++ API/Dtos/BasketItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Dtos
+{
+    public class BasketItemDto 
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+        public string PictureUrl { get; set; } 
+    }
+}
---
+++ API/Dtos/CustomerBasketDto.cs
@@ -0,0 +1,13 @@
+﻿using Core.Enitities;
+
+namespace API.Dtos
+{
+    public class CustomerBasketDto
+    {
+        public string Id { get; set; }
+        public int CustomerId { get; set; }
+        public string ClientSecret { get; set; }
+        public string PaymentIntentId { get; set; }
+        public List<BasketItem> Items { get; set; }
+    }
+}
