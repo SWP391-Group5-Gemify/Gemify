f4eb0dc71bd4b5b98506dc99419793aa8249c1e9
meewaldor <tieumylam91@gmail.com>
2024-06-12 22:13:43 +0700

    My - Added create buy-back order #106

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,16 +1,22 @@
-﻿using Core.Enitities.OrderAggregate;
+﻿using Core.Enitities;
+using Core.Enitities.OrderAggregate;
 using Core.Interfaces;
+using Core.Specifications;
+using Core.Specifications.Customers;
 using Core.Specifications.Orders;
+using Infrastructure.Data;
 
 namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly BasketRepository _basketRepository;
 
-        public OrderService(IUnitOfWork unitOfWork)
+        public OrderService(IUnitOfWork unitOfWork, BasketRepository basketRepository)
         {
             _unitOfWork = unitOfWork;
+            _basketRepository = basketRepository;
         }
 
         /**
@@ -29,9 +35,29 @@ namespace Infrastructure.Services
          *         CREATE BUYBACK ORDER
          * =================================
         **/
-        public Task<Order> CreateBuyBackOrderAsync()
+        public async Task<Order> CreateBuyBackOrderAsync(string basketId, int customerId, int repurchaserId)
         {
-            throw new NotImplementedException();
+            // add customer (if not exist)
+            var customer = _unitOfWork.Repository<Customer>().GetByIdAsync(customerId);
+
+            // get basket
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+
+            // add items in basket
+            List<Product> productList = new List<Product>();
+            if (basket != null)
+            {
+                foreach (var item in basket.Items)
+                {
+                    var product = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+
+                    var itemOrdered = new ProductItemOrdered(product.Id, product.Name, 0, product.GoldType.Name, product.GoldWeight
+                        , 0, product.SubCategory.Unit, product.ImageUrl);
+                    var orderItem = new OrderItem(itemOrdered, 0, 1);
+
+                    productList.Add(product);
+                }
+            }
         }
 
 
