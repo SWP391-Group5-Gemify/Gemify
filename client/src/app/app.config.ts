9fc5b59917b3e8089e0cd92732a8b230a0424381
meewaldor <tieumylam91@gmail.com>
2024-06-19 21:47:34 +0700

    My - Added file upload method #100

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -5,11 +5,19 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { httpInterceptorInterceptor } from './core/interceptors/http-interceptor.interceptor';
+import { importProvidersFrom } from '@angular/core';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { provideStorage, getStorage} from '@angular/fire/storage';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([httpInterceptorInterceptor])),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideStorage(() => getStorage()),
+    ])
   ],
 };
