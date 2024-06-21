20270635f936b496a12ec56f98e1a3f58b6fb546
SE182407_VuKimDuy <duyvkse182407@fpt.edu.vn>
2024-06-22 01:46:18 +0700

    Duy - Fix loading baskets on 3 roles only #103

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { FooterComponent } from "./core/layout/footer/footer.component";
-import { HeaderComponent } from "./core/layout/header/header.component";
-import { BasketService } from "./core/services/basket/basket.service";
-import { AuthService } from "./core/services/auth/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { BasketService } from './core/services/basket/basket.service';
+import { AuthService } from './core/services/auth/auth.service';
+import { RoleEnum } from './core/models/role.model';
 
 @Component({
-  selector: "app-root",
+  selector: 'app-root',
   standalone: true,
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class AppComponent implements OnInit {
-  title = "client";
+  title = 'client';
 
   constructor(
     private basketService: BasketService,
@@ -27,9 +28,20 @@ export class AppComponent implements OnInit {
   /**
    * Loading the Basket on basket_id on Local Storage
    * - Must have JWT, or else does not load the basket from redis
+   * - Cashier, Seller, Repurchasher are only 3 roles to load baskets
    */
   loadBasketOnBasketIdLocalStorage() {
-    if (this.authService.token) {
+    const allowedRoles: RoleEnum[] = [
+      RoleEnum.Cashier,
+      RoleEnum.Seller,
+      RoleEnum.Repurchaser,
+    ];
+
+    let isAllowedToLoadBaskets: boolean =
+      allowedRoles.includes(this.authService.currentUser?.role as RoleEnum) &&
+      this.authService.token != null;
+
+    if (isAllowedToLoadBaskets) {
       const basketId = this.basketService.currentBasketId;
       basketId && this.basketService.loadCurrentBasket(basketId);
     }
