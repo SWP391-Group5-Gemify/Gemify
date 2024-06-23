8ad3a380ad9f0e388f3e1b970cb682f916ac8cdf
SE182407_VuKimDuy <duyvkse182407@fpt.edu.vn>
2024-06-23 21:12:04 +0700

    Duy - Resolve conflict from previous merge #153

--- client/src/app/core/services/notification/notification.service.spec.ts
+++ client/src/app/core/services/notification/notification.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { NotificationService } from './snackbar.service';
+import { NotificationService } from './notification.service';
 
 describe('SnackbarService', () => {
   let service: NotificationService;
