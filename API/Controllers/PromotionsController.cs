abc3c59d6379ee2c34a6ad623a4aabf94d72edcc
khanhcnp <khanhcnpse181509@fpt.edu.vn>
2024-06-24 21:52:36 +0700

    Khanh Cao - Added PromotionService #135

--- API/Controllers/PromotionsController.cs
+++ API/Controllers/PromotionsController.cs
@@ -4,16 +4,16 @@ using Core.Specifications.Promotions;
 using API.Errors;
 using API.Helpers;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; 
 
 namespace API.Controllers
 {
     public class PromotionsController : BaseApiController
     {
-        private readonly IGenericRepository<Promotion> _promotionRepo;
-        public PromotionsController(IGenericRepository<Promotion> promotionRepo)
+        private readonly IPromotionService _promotionService;
+        public PromotionsController(IPromotionService promotionService)
         {
-            _promotionRepo = promotionRepo;
+            _promotionService = promotionService;
         }
 
         // Get promotions with spec
@@ -23,28 +23,10 @@ namespace API.Controllers
         {
             var spec = new PromotionSpecification(promotionParams);
             var countSpec = new PromotionCountSpecification(promotionParams);
-            var totalPromotions = await _promotionRepo.CountAsync(countSpec);
-            var promotions = await _promotionRepo.ListAsync(spec);
-
-            // Update status for expired promotions
-            if (promotions != null)
-            {
-                DateOnly today = DateOnly.FromDateTime(DateTime.UtcNow);
-                int countExpiredPromotions = 0;
-                foreach (var promotion in promotions)
-                {
-                    if (promotion.Status == true && today.CompareTo(promotion.ExpDate) > 0)
-                    {
-                        promotion.Status = false;
-                        _promotionRepo.Update(promotion);
-                        countExpiredPromotions++;
-                    }
-                }
-                if (countExpiredPromotions > 0) await _promotionRepo.SaveAllAsync();
-                return Ok(new Pagination<Promotion>
-                    (promotionParams.PageIndex, promotionParams.PageSize, totalPromotions, promotions));
-            }
-            else return BadRequest(new ApiResponse(400, "Cannot retrieve promotions list!"));
+            var totalPromotions = await _promotionService.CountPromotionsWithSpecAsync(countSpec);
+            var promotions = await _promotionService.GetPromotionsWithSpecAsync(spec);
+            return Ok(new Pagination<Promotion>
+                (promotionParams.PageIndex, promotionParams.PageSize, totalPromotions, promotions));
         }
 
         // Get indate promotion by code
@@ -52,40 +34,34 @@ namespace API.Controllers
         [Authorize(Roles = "StoreOwner,StoreManager,Cashier,Seller")]
         public async Task<ActionResult<Promotion>> GetPromotionByCode(string code)
         {
-            var spec = new PromotionSpecification(code);
-            var exist_promotion = await _promotionRepo.GetEntityWithSpec(spec);
-            if(exist_promotion == null) return NotFound(new ApiResponse(404, "Promotion not found!"));
+            var exist_promotion = await _promotionService.GetPromotionByCodeAsync(code);
+            if(exist_promotion == null) 
+                return NotFound(new ApiResponse(404, "Promotion is expired or does not exist!"));
             return Ok(exist_promotion);
         }
 
         // Add new promotion
         [HttpPost]
         [Authorize(Roles = "StoreOwner")]
-        public async Task<ActionResult> AddPromotion(Promotion promotion)
+        public async Task<ActionResult<Promotion>> AddPromotion(Promotion promotion)
         {
-            if (promotion != null && promotion.ExpDate.CompareTo(promotion.EffDate) > 0)
-            {
-                    _promotionRepo.Add(promotion);
-                    var result = await _promotionRepo.SaveAllAsync();
-                    if (result) return Ok(new ApiResponse(200, "New promotion added successfully!"));
-                    else return BadRequest(new ApiResponse(400, "Failed to add new promotion!"));
-            }
-            else return BadRequest(new ApiResponse(400, "Failed to add new promotion!"));
+            var newPromotion = await _promotionService.AddNewPromotionAsync(promotion);
+            if(newPromotion == null) return BadRequest(new ApiResponse(400, "Failed to add new promotion!"));
+            return newPromotion;
         }
 
         // Disable promotion manually
-        [HttpDelete]
+        [HttpDelete("{id}")]
         [Authorize(Roles = "StoreOwner")]
-        public async Task<ActionResult> DisablePromotionByCode(string code)
+        public async Task<ActionResult> DisablePromotionById(int id)
         {
-            var spec = new PromotionSpecification(code);
-            var exist_promotion = await _promotionRepo.GetEntityWithSpec(spec);
-            if(exist_promotion == null) return BadRequest(new ApiResponse(400, "Failed to disable promotion!"));
-            exist_promotion.Status = false;
-            _promotionRepo.Update(exist_promotion);
-            var result = await _promotionRepo.SaveAllAsync();
-            if(result) return Ok(new ApiResponse(200,"Disabled promotion successfully!"));
-            else return BadRequest(new ApiResponse(400,"Failed to disable promotion!"));
+            var exist_promotion = await _promotionService.GetPromotionByIdAsync(id);
+            if(exist_promotion != null)
+            {
+                await _promotionService.UpdateExpiredPromotionAsync(exist_promotion);
+                return Ok(new ApiResponse(200, "Disabled promotion successfully!"));
+            }
+            else return BadRequest(new ApiResponse(400, "Failed to disable promotion!"));
         }
     }
 }
