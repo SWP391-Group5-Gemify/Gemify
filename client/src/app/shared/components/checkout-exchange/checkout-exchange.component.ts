d0134278f97ad4a842358b579a5b670c6c25e381
r1nzl3rrr <khanhle9344@gmail.com>
2024-07-12 11:31:25 +0700

    checkout exchange

---
+++ client/src/app/shared/components/checkout-exchange/checkout-exchange.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-exchange',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkout-exchange.component.html',
+  styleUrl: './checkout-exchange.component.scss'
+})
+export class CheckoutExchangeComponent {
+
+}
